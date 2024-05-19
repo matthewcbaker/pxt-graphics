0631b950046898b40fb60c83563514e6c4f51fc2
Matt Baker <mbaker@itrsgroup.com>
2024-05-19 16:48:26 +0100

    Added a canvas

---
+++ custom.ts
@@ -0,0 +1,19 @@
+
+/**
+ * Graphics assistance
+ */
+//% weight=10 color="#de26a7" icon="\uf302"
+namespace graphics {
+
+    //%
+    export class Canvas { }
+
+    /**
+     * Creates a canvas for use in a variable
+     */
+    //% block="create canvas $width by $height"
+    //% blockSetVariable=canvas
+    export function createCanvas(width: number, height: number): Canvas {
+        return undefined;
+    }
+}
