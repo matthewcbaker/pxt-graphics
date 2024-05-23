355836f0313530e61144257dbd88dd3e3123c6d1
Matt Baker <mbaker@itrsgroup.com>
2024-05-23 23:02:56 +0100

    Added grouping (and moved a function to force the group to be first)

--- custom.ts
+++ custom.ts
@@ -3,7 +3,20 @@
  * Graphics assistance
  */
 //% weight=10 color="#de26a7" icon="\uf302"
+//% groups=['Canvas', 'Drawing', 'Window']
 namespace graphics {
+
+    /**
+     * Creates a canvas for use in a variable
+     */
+    //% block="create canvas $width by $height"
+    //% group="Canvas"
+    //% width.min=1 width.max=1024 width.defl=40
+    //% height.min=1 height.max=768 height.defl=30
+    //% blockSetVariable=canvas
+    export function createCanvas(width: number, height: number): Canvas {
+        return new Canvas();
+    }
 
     //%
     export class Canvas {
@@ -14,6 +27,7 @@ namespace graphics {
          */
         //% block="add sprite to $this"
         //% blockSetVariable=sprite
+        //% group="Drawing"
         //% weight=8
         public createSprite(): Sprite {
             return new Sprite();
@@ -26,6 +40,7 @@ namespace graphics {
          * Gets the differences between the last change request and this one.
          */
         //% block="get changes to $this"
+        //% group="Window"
         //% weight=6
         public getChanges(): void {
         }
@@ -37,6 +52,7 @@ namespace graphics {
          * Replace the current sprite graphics with a basic image.
          */
         //% block="replace $this image with $image"
+        //% group="Drawing"
         //% weight=6
         public setImage(image: Image): void {
         }
@@ -49,20 +65,10 @@ namespace graphics {
     export class Pixel { }
 
     /**
-     * Creates a canvas for use in a variable
-     */
-    //% block="create canvas $width by $height"
-    //% width.min=1 width.max=1024 width.defl=40
-    //% height.min=1 height.max=768 height.defl=30
-    //% blockSetVariable=canvas
-    export function createCanvas(width: number, height: number): Canvas {
-        return new Canvas();
-    }
-
-    /**
      * Creates a window to view a section of canvas.
      */
     //% block="create window to view $canvas"
+    //% group="Window"
     //% blockSetVariable=window
     //% weight=10
     export function createWindow(canvas: Canvas): Window {
