51f597b23d1595446eeeeeed831a761427202c69
Matt Baker <mbaker@itrsgroup.com>
2024-06-08 15:40:48 +0100

    Further test arrangements

--- test.ts
+++ test.ts
@@ -1,11 +1,16 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 class Assert {
     static _failures: number = 0
+    static _current: string = ""
 
     static _assertionFail(name: string, actual: string, expected: string) {
         Assert._failures += 1
-        console.log("" + name + ": " + actual + " != " + expected)
+        console.log("" + Assert._current + ":" + name + ": " + actual + " != " + expected)
         
+    }
+
+    static setCurrent(current: string) {
+        Assert._current = current
     }
 
     static assertNumber(name: string, test: number, expected: number) {
@@ -24,6 +29,7 @@ class Assert {
 }
 
 function testInitialiseSprite() {
+    Assert.setCurrent("testInitialiseSprite")
     let canvas = graphics.createCanvas(40, 30)
     let sprite = canvas.createSprite()
     Assert.assertNumber("sprite width init", sprite.width, 0)
@@ -32,17 +38,18 @@ function testInitialiseSprite() {
 testInitialiseSprite()
 
 function testSettingSpriteImage() {
+    Assert.setCurrent("testSettingSpriteImage")
     let canvas = graphics.createCanvas(40, 30)
     let sprite = canvas.createSprite()
     sprite.setImage(images.iconImage(IconNames.Heart))
     Assert.assertNumber("sprite image width", sprite.width, 5)
     Assert.assertNumber("sprite image height", sprite.height, 5)
-    Assert.assertNumber("sprite pixel 0,0", sprite.pixel(0, 0).colour.red, 0)
-    Assert.assertNumber("sprite pixel 0,0", sprite.pixel(0, 0).colour.green, 0)
-    Assert.assertNumber("sprite pixel 0,0", sprite.pixel(0, 0).colour.blue, 0)
-    Assert.assertNumber("sprite pixel 0,1", sprite.pixel(0, 1).colour.red, 255)
-    Assert.assertNumber("sprite pixel 0,1", sprite.pixel(0, 1).colour.green, 255)
-    Assert.assertNumber("sprite pixel 0,1", sprite.pixel(0, 1).colour.blue, 255)
+    Assert.assertNumber("sprite pixel 0,0,r", sprite.pixel(0, 0).colour.red, 0)
+    Assert.assertNumber("sprite pixel 0,0,g", sprite.pixel(0, 0).colour.green, 0)
+    Assert.assertNumber("sprite pixel 0,0,b", sprite.pixel(0, 0).colour.blue, 0)
+    Assert.assertNumber("sprite pixel 0,1,r", sprite.pixel(0, 1).colour.red, 255)
+    Assert.assertNumber("sprite pixel 0,1,g", sprite.pixel(0, 1).colour.green, 255)
+    Assert.assertNumber("sprite pixel 0,1,b", sprite.pixel(0, 1).colour.blue, 255)
     Assert.assertNumber("sprite pixel 0,0", sprite.pixel(0, 0).colour.brightness, 0)
     Assert.assertNumber("sprite pixel 0,1", sprite.pixel(0, 1).colour.brightness, 255)
     Assert.assertNumber("sprite pixel 0,2", sprite.pixel(0, 2).colour.brightness, 255)
@@ -56,6 +63,7 @@ function testSettingSpriteImage() {
 testSettingSpriteImage()
 
 function testSpriteSetImage() {
+    Assert.setCurrent("testSpriteSetImage")
     let canvas = graphics.createCanvas(40, 30)
     let sprite = canvas.createSprite()
     sprite.setImage(images.iconImage(IconNames.Heart))
@@ -67,6 +75,7 @@ function testSpriteSetImage() {
 testSpriteSetImage()
 
 function testInitialChangesWhenBlank() {
+    Assert.setCurrent("testInitialChangesWhenBlank")
     let canvas = graphics.createCanvas(40, 30)
     let window = graphics.createWindow(canvas)
     Assert.assertNumber("no changes", window.getChanges().length, 0)
@@ -74,6 +83,7 @@ function testInitialChangesWhenBlank() {
 testInitialChangesWhenBlank()
 
 function testInitialChangesWhenBlankSprite() {
+    Assert.setCurrent("testInitialChangesWhenBlankSprite")
     let canvas = graphics.createCanvas(40, 30)
     let window = graphics.createWindow(canvas)
     let sprite = canvas.createSprite()
